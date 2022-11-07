cca89d3b48b9c11307e20c3c41190245ba0b456e
EqualMa <equalma@outlook.com>
2022-11-07 18:33:45 +0800

    feat: impl Default and Debug for Ref and RefWith

--- packages/hooks/src/ref_hook/hook.rs
+++ packages/hooks/src/ref_hook/hook.rs
@@ -1,14 +1,39 @@
-use hooks_core::HookExt;
-
 use super::SharedRef;
 
-pub struct RefWith<T>(Option<SharedRef<T>>);
+#[derive(Debug)]
+pub struct Ref<T>(Option<SharedRef<T>>);
+
+impl<T> Default for Ref<T> {
+    #[inline]
+    fn default() -> Self {
+        Self(None)
+    }
+}
+
+impl<T> Unpin for Ref<T> {}
+
+#[derive(Debug)]
+pub struct RefWith<T>(Ref<T>);
+
+impl<T> Default for RefWith<T> {
+    #[inline]
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
 
 impl<T> Unpin for RefWith<T> {}
 
-pub struct Ref<T>(RefWith<T>);
-
-impl<T> Unpin for Ref<T> {}
+impl<T> Ref<T> {
+    pub fn use_hook_with(
+        self: std::pin::Pin<&mut Self>,
+        get_initial_value: impl FnOnce() -> T,
+    ) -> &SharedRef<T> {
+        self.get_mut()
+            .0
+            .get_or_insert_with(move || SharedRef::new(get_initial_value()))
+    }
+}
 
 crate::utils::impl_hook! {
     impl [T] for Ref<T> {
@@ -18,7 +43,7 @@ crate::utils::impl_hook! {
         }
         #[inline]
         use_hook(self, initial_value: T) -> &'hook SharedRef<T> {
-            self.get_mut().0.use_hook((move || initial_value,))
+            self.use_hook_with(move || initial_value)
         }
     }
 }
@@ -31,17 +56,17 @@ crate::utils::impl_hook! {
         }
         #[inline]
         use_hook[F: FnOnce() -> T](self, get_initial_value: F) -> &'hook SharedRef<T> {
-            self.get_mut().0.get_or_insert_with(move || SharedRef::new(get_initial_value()))
+            std::pin::Pin::new(&mut self.get_mut().0).use_hook_with(get_initial_value)
         }
     }
 }
 
 #[inline]
 pub fn use_ref<T>() -> Ref<T> {
-    Ref(use_ref_with())
+    Default::default()
 }
 
 #[inline]
-fn use_ref_with<T>() -> RefWith<T> {
-    RefWith(None)
+pub fn use_ref_with<T>() -> RefWith<T> {
+    Default::default()
 }
