11f06aea4cd32fcb243946f407c03c774116cb96
EqualMa <equalma@outlook.com>
2022-11-08 21:01:28 +0800

    chore: deny undocumented_unsafe_blocks for hooks-core

--- packages/hooks-core/src/lib.rs
+++ packages/hooks-core/src/lib.rs
@@ -1,10 +1,10 @@
+#![deny(clippy::undocumented_unsafe_blocks)]
+
 mod ext;
 mod hook;
-mod run_hook;
 
 pub use ext::*;
 pub use hook::*;
-pub use run_hook::*;
 
 pub mod fn_hook;
 pub mod hook_pair;
