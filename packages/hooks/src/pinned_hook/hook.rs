b2d14c30c90eaaa89b908426a37818fb493ce835
EqualMa <equalma@outlook.com>
2022-11-12 15:14:44 +0800

    feat: use_default_pinned_hook now also accepts one argument inferring hook type

--- packages/hooks/src/pinned_hook/hook.rs
+++ packages/hooks/src/pinned_hook/hook.rs
@@ -1,6 +1,6 @@
 use std::pin::Pin;
 
-use hooks_core::HookPollNextUpdate;
+use hooks_core::{Hook, HookLifetime, HookPollNextUpdate};
 
 pin_project_lite::pin_project! {
     #[derive(Debug, Default)]
@@ -30,6 +30,25 @@ crate::utils::impl_hook! {
     }
 }
 
+impl<'hook, H: HookPollNextUpdate + Default, F: FnOnce() -> H> HookLifetime<'hook, (F,)>
+    for DefaultPinnedHook<H>
+{
+    type Value = Pin<&'hook mut H>;
+}
+
+impl<H: HookPollNextUpdate + Default, F: FnOnce() -> H> Hook<(F,)> for DefaultPinnedHook<H> {
+    #[inline]
+    fn use_hook<'hook>(
+        self: Pin<&'hook mut Self>,
+        _: (F,),
+    ) -> <Self as HookLifetime<'hook, (F,)>>::Value
+    where
+        Self: 'hook,
+    {
+        self.use_hook(())
+    }
+}
+
 /// Use another hook by just [`poll_next_update`] without calling [`use_hook`].
 /// This allows conditionally using hooks.
 /// See also [`use_lazy_pinned_hook`](crate::use_lazy_pinned_hook).
@@ -38,11 +57,28 @@ crate::utils::impl_hook! {
 /// [`use_hook`]: crate::Hook::use_hook
 ///
 /// ```
-/// # use hooks::{hook, use_default_pinned_hook, use_effect, use_state, Hook};
+/// # use hooks::{hook, use_default_pinned_hook, use_state, Hook};
 /// #[hook]
 /// fn use_demo() -> i32 {
 ///     let (state, updater) = use_state(0);
 ///     let hook_effect = use_default_pinned_hook::<hooks::Effect<i32, _>>();
+///     if *state < 2 {
+///         let updater = updater.clone();
+///         hook_effect.use_hook((move |v: &i32| updater.set(*v + 1), *state));
+///     }
+///     *state
+/// }
+/// ```
+///
+/// You can also pass a fn instead of specifying the hook type.
+/// The fn is just used to infer the type, and it will not be called.
+///
+/// ```
+/// # use hooks::{hook, use_default_pinned_hook, use_effect, use_state, Hook};
+/// #[hook]
+/// fn use_demo() -> i32 {
+///     let (state, updater) = use_state(0);
+///     let hook_effect = use_default_pinned_hook(use_effect);
 ///     if *state < 2 {
 ///         let updater = updater.clone();
 ///         hook_effect.use_hook((move |v: &i32| updater.set(*v + 1), *state));
