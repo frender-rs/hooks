33e86122a3edcfc3cc45fe406038e3d714d199b1
EqualMa <equalma@outlook.com>
2022-12-15 15:42:19 +0800

    test: inherit multi

---
+++ packages/hooks-frender/tests/inherit_multi.rs
@@ -0,0 +1,53 @@
+use std::any::Any;
+
+use hooks_frender::{build, def_props};
+
+pub trait MaybeEventListener {
+    fn handle_event(&mut self, event: &dyn Any);
+}
+
+impl MaybeEventListener for () {
+    fn handle_event(&mut self, _: &dyn Any) {}
+}
+
+impl<F> MaybeEventListener for F
+where
+    F: FnMut(&dyn Any),
+{
+    fn handle_event(&mut self, _: &dyn Any) {
+        // do something
+    }
+}
+
+def_props! {
+    pub struct EventTargetProps {
+        on_change[impl MaybeEventListener]: () = (),
+        on_click [impl MaybeEventListener]: () = (),
+    }
+}
+
+def_props! {
+    pub struct AriaProps {
+        aria_hidden[? bool],
+        aria_label [borrow? str],
+    }
+}
+
+def_props! {
+    pub struct ElementProps {
+        event_listeners[inherit EventTargetProps],
+        aria[inherit AriaProps],
+    }
+}
+
+#[test]
+fn build() {
+    let mut triggered_events = vec![];
+    let mut props = build!(ElementProps {
+        on_change: |_: &_| triggered_events.push("change"),
+        aria_hidden: true,
+    });
+    assert!(props.aria.aria_hidden);
+    (props.event_listeners.on_change)(&());
+    assert_eq!(triggered_events, ["change"]);
+}
