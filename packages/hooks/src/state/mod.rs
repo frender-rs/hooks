08060a0141dee8a550b57faab72745bd695c0376
Equal Ma <equalma@outlook.com>
2023-03-08 18:11:23 +0800

    wip(v2): test use_state

--- packages/hooks/src/state/mod.rs
+++ packages/hooks/src/state/mod.rs
@@ -11,7 +11,7 @@ pub use use_hook::*;
 #[cfg(test)]
 mod tests {
     use futures_lite::StreamExt;
-    use hooks_core::{fn_hook, hook_fn, Hook, HookPollNextUpdateExt};
+    use hooks_core::{hook_fn, Hook, HookPollNextUpdateExt};
     use hooks_derive::hook;
 
     use super::{use_state, use_state_with, STAGING_STATES_DEFAULT_STACK_COUNT};
@@ -40,6 +40,8 @@ mod tests {
     #[cfg(feature = "use_effect")]
     #[test]
     fn hook_fn_state_2() {
+        use hooks_core::IntoHook;
+
         hook_fn!(
             fn use_state_2() -> (i32, i32) {
                 let (state_1, updater_1) = h!(use_state(1));
@@ -65,7 +67,7 @@ mod tests {
         );
 
         futures_lite::future::block_on(async {
-            let values = use_state_2().into_iter().collect::<Vec<_>>().await;
+            let values = use_state_2().into_hook_values().collect::<Vec<_>>().await;
 
             assert_eq!(values, [(1, 2), (2, 3), (3, 5), (5, 8), (8, 13)]);
         });
